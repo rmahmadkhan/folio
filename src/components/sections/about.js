a2fbaecb87947df9b25b2d7c11418accfd05bee3
Ahmad Khan <rmahmadkhan@icloud.com>
2023-07-08 15:42:05 +0500

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Flutter', 'Firebase', 'iOS', 'Android'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,30 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Ahmad. While I have a comprehensive technical background,
+              my emphasis is on mobile app development. I have been working as a software engineer since 2020
+              and have developed a wide range of mobile apps for iOS and Android.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Fast-forward to today, and I’ve had the privilege of working with multiple startups.
+              To build products which have high user experience and performance has always been my top priority.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Besides that, I'm a passionate community builder. For that, I have been attached to{' '}
+              <a href="https://developers.google.com/">Google for Developers</a>{' '}
+              since 2020 as a community organizer.
+              Currently, I am co-managing{' '}
+              <a href="https://developers.google.com/">GDG Live Pakistan</a>{' '}
+              where we are on a mission to connect digital Pakistan.
+              I'm also the founder of{' '}
+              <a href="https://developers.google.com/">Flutter Lahore</a>,{' '}
+              where we are building a local community for developers in Lahore who are interested in{' '}
+              <a href="https://developers.google.com/">Flutter</a>.{' '}
+              Previously, I led{' '}
+              <a href="https://developers.google.com/">GDSC PUCIT</a>{' '}
+              at my campus where I managed a team to build a tech community in my university.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
