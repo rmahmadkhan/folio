38ab36c9c985834794400f37bb443bf5df04e2b6
Ahmad Khan <rmahmadkhan@icloud.com>
2023-07-08 16:55:20 +0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/bchiang7/">
-          <div>Special thanks to Brittany Chiang</div>
+          <div>Inspired from Brittany Chiang</div>
         </a>
       </StyledCredit>
     </StyledFooter>
