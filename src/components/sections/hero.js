a24e3e989725a0307d36c22d1ac2cbb77c96bc26
Ahmad Khan <rmahmadkhan@icloud.com>
2023-07-08 16:20:55 +0500

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Ahmad Khan</h2>;
-  const three = <h3 className="big-heading">I code, travel &amp; build communities.</h3>;
+  const three = <h3 className="medium-heading">I code, travel &amp; build communities.</h3>;
   const four = (
     <>
       <p>
