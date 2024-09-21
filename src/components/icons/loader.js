db7a5b9c007caa588a81e467a3bd4b47b9421d33
Ahmad Khan <rmahmadkhan@icloud.com>
2024-09-22 04:41:06 +0800

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -4,12 +4,21 @@ const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
+      {/* Outer A */}
       <g id="A" transform="translate(11.000000, 5.000000)">
         <path
-          d="M25,60 L50,10 L75,60 Z" // A
+          d="M50 10 L35 70 L40 70 L44 55 L56 55 L60 70 L65 70 L50 10 Z" // Outer part of A
           fill="currentColor"
         />
+        {/* Inner triangle */}
+        <path
+          d="M47 50 L53 50 L50 35 Z" // Inner triangle
+          fill="none" // Hollow
+          stroke="currentColor" // Keep the outline visible
+        />
       </g>
+
+      {/* Hexagonal outer shape */}
       <path
         stroke="currentColor"
         strokeWidth="5"
